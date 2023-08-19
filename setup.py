f251c07dde2f522cabe20bfc4e4515ffe7a3458f
Steve Phelps <sphelps@sphelps.net>
2023-08-19 13:37:09 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="zipline-tardis-bundle",
+    version="0.1.0",
+    packages=[],
+    url="",
+    license="MIT",
+    author="Steve Phelps",
+    author_email="sphelps@sphelps.net",
+    description="Zipline bundle to ingest TARDIS data",
+)
