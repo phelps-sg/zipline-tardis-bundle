5dd156390f12c17f9002193be277af927a180c46
Steve Phelps <sphelps@sphelps.net>
2023-08-19 17:45:43 +0100

    use py_module convention

--- setup.py
+++ setup.py
@@ -3,8 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="zipline-tardis-bundle",
     version="0.1.0",
-    packages=find_packages(),
-    url="",
+    py_modules=['zipline_tardis_bundle'],
     license="MIT",
     author="Steve Phelps",
     author_email="sphelps@sphelps.net",
