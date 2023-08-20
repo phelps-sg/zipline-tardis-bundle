c718b7dcb01c373c261d38be5876229fdc219586
Steve Phelps <sphelps@sphelps.net>
2023-08-20 20:08:32 +0100

    remove unused import

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name="zipline-tardis-bundle",
     version="0.1.0",
     packages=['zipline_tardis_bundle'],
-    license="MIT",
+    license="Apache License, Version 2.0",
     author="Steve Phelps",
     author_email="sphelps@sphelps.net",
     description="Zipline bundle to ingest TARDIS data"
