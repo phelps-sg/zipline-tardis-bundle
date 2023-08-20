405f63442554d57dfce8b5ff17a209404ae2d35e
Steve Phelps <sphelps@sphelps.net>
2023-08-20 13:01:00 +0100

    revert install_requires

--- setup.py
+++ setup.py
@@ -8,4 +8,5 @@ setup(
     author="Steve Phelps",
     author_email="sphelps@sphelps.net",
     description="Zipline bundle to ingest TARDIS data",
+    install_requires=['mPyPl', 'tardis-client', 'tardis_dev']
 )
