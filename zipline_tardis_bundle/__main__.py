b35fb1bfefa199e3ce2a0e5d47254c097dd7d129
Steve Phelps <sphelps@sphelps.net>
2023-08-23 12:33:43 +0100

    correct copyright year

--- zipline_tardis_bundle/__main__.py
+++ zipline_tardis_bundle/__main__.py
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2024 Steve Phelps.
+# Copyright (C) 2023 Steve Phelps.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
